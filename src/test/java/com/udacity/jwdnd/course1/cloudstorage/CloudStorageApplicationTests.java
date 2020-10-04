edb7d0aeb9d3f39e0f4361169b9f8fa64f08f024
yuxiRen <rita.ren.yuxi@gmail.com>
2020-10-03 23:54:20 -0700

    note editing test

--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -24,8 +24,10 @@ class CloudStorageApplicationTests {
 	private static String userName = "reny";
 	private static String password = "123";
 	private static String noteTitle = "example note";
+	private static String newNoteTitle = "new Note";
 	private static String noteDescription = "example note description";
 	private static String credentialURL = "www.exampleCredentialURL.com";
+
 	@LocalServerPort
 	private int port;
 
@@ -64,19 +66,11 @@ class CloudStorageApplicationTests {
 		WebElement signUpButton = driver.findElement(By.id("signup"));
 		signUpButton.click();
 		//Login
-		driver.get("http://localhost:" + this.port + "/login");
-		inputUsername = driver.findElement(By.id("inputUsername"));
-		inputUsername.sendKeys(userName);
-		inputPassword = driver.findElement(By.id("inputPassword"));
-		inputPassword.sendKeys(password);
-		WebElement loginButton = driver.findElement(By.id("login"));
-		loginButton.click();
-		Assertions.assertEquals("Home", driver.getTitle());
-
+		getLoginPageTest();
 		//Logout
 		WebElement logoutButton = driver.findElement(By.id("logout"));
 		logoutButton.click();
-		Thread.sleep(1000);
+		Thread.sleep(2000);
 		Assertions.assertEquals(true, driver.getCurrentUrl().contains("login?logout"));
 		//Unauthorized Access Restrictions
 		getUnauthorizedHomePageTest();
@@ -88,10 +82,17 @@ class CloudStorageApplicationTests {
 		Assertions.assertEquals("Sign Up", driver.getTitle());
 	}
 
-	@Test
-	public void getLoginPageTest() {
+
+	public void getLoginPageTest() throws InterruptedException {
 		driver.get("http://localhost:" + this.port + "/login");
-		Assertions.assertEquals("Login", driver.getTitle());
+		WebElement inputUsername = driver.findElement(By.id("inputUsername"));
+		inputUsername.sendKeys(userName);
+		WebElement inputPassword = driver.findElement(By.id("inputPassword"));
+		inputPassword.sendKeys(password);
+		WebElement loginButton = driver.findElement(By.id("login"));
+		loginButton.click();
+		Thread.sleep(2000);
+		Assertions.assertEquals("Home", driver.getTitle());
 	}
 	@Test
 	public void getUnauthorizedHomePageTest() {
@@ -104,14 +105,7 @@ class CloudStorageApplicationTests {
 		WebDriverWait wait = new WebDriverWait (driver, 30);
 		JavascriptExecutor jse =(JavascriptExecutor) driver;
 		//Login
-		driver.get("http://localhost:" + this.port + "/login");
-		WebElement inputUsername = driver.findElement(By.id("inputUsername"));
-		inputUsername.sendKeys(userName);
-		WebElement inputPassword = driver.findElement(By.id("inputPassword"));
-		inputPassword.sendKeys(password);
-		WebElement loginButton = driver.findElement(By.id("login"));
-		loginButton.click();
-		Assertions.assertEquals("Home", driver.getTitle());
+		getLoginPageTest();
 
 		//Note Creation
 		WebElement notesTab = driver.findElement(By.id("nav-notes-tab"));
@@ -127,9 +121,6 @@ class CloudStorageApplicationTests {
 		Assertions.assertEquals("Home", driver.getTitle());
 
 		//Note Viewing
-		driver.get("http://localhost:" + this.port + "/home");
-		notesTab = driver.findElement(By.id("nav-notes-tab"));
-		jse.executeScript("arguments[0].click()", notesTab);
 		WebElement notesTable = driver.findElement(By.id("userTable"));
 		List<WebElement> notesList = notesTable.findElements(By.tagName("th"));
 		Boolean created = false;
@@ -141,5 +132,37 @@ class CloudStorageApplicationTests {
 			}
 		}
 		Assertions.assertTrue(created);
+
+		//Note Editing
+		List<WebElement> notesOptionList = notesTable.findElements(By.tagName("td"));
+		WebElement editElement = null;
+		for (int i = 0; i < notesOptionList.size(); i++) {
+			WebElement element = notesOptionList.get(i);
+			editElement = element.findElement(By.id("edit"));
+			if (editElement != null){
+				break;
+			}
+		}
+		wait.until(ExpectedConditions.elementToBeClickable(editElement)).click();
+		WebElement noteTitle = driver.findElement(By.id("note-title"));
+		wait.until(ExpectedConditions.elementToBeClickable(noteTitle));
+		noteTitle.clear();
+		noteTitle.sendKeys(newNoteTitle);
+		savechanges = driver.findElement(By.id("save-changes"));
+		savechanges.click();
+		Assertions.assertEquals("Home", driver.getTitle());
+
+		//Note Editing Check
+		notesTable = driver.findElement(By.id("userTable"));
+		notesList = notesTable.findElements(By.tagName("th"));
+		Boolean editeSuccess = false;
+		for (int i = 0; i < notesList.size(); i++) {
+			WebElement element = notesList.get(i);
+			if (element.getAttribute("innerHTML").equals(newNoteTitle)) {
+				editeSuccess = true;
+				break;
+			}
+		}
+		Assertions.assertTrue(editeSuccess);
 	}
 }
